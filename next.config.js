75722bbdb7613a60d1369ff3e97e655529574be1
Ratheshan03 <lionratheshan@gmail.com>
2021-07-28 19:49:33 +0530

    testing error

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   reactStrictMode: true,
-
   images: {
     domains: ["links.papareact.com", "image.tmdb.org"],
+  },
+  variants: {
+    extend: {
+      animation: ["group-hover", "hover", "focus"],
+    },
   },
 };
